4fe7a490811182389ba363f2e2b3cc4e1c3ce0f5
Daniel Schulenberg <dschumil@gmail.com>
2022-06-26 12:42:45 -0700

    all final changes in code

--- src/com/skilldistrillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistrillery/foodtruck/app/FoodTruckApp.java
@@ -6,31 +6,126 @@ import com.skilldistrillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
 
-	private FoodTruck [] FleetOfFoodTruck;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private FoodTruck[] FleetOfFoodTruck = new FoodTruck[5];
+	private int count = 0;
 
-		
-		
-		
-		
+	public static void main(String[] args) {
+
+		FoodTruckApp app = new FoodTruckApp();
+
+		app.run();
+
 	}
-	
-	public void userInput() {
+
+	private void run() {
+		FoodTruck newft = null;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Input the name of the food truck:");
-		
-		System.out.println("Input the type of food of the food truck:");
-		
-		System.out.println("Input the rating of the food truck:");
+		boolean start = true;
+
+		while (start && count < 5) {
+			System.out.println("Input the name of the food truck or type 'quit' to stop:");
+			String name = scan.next();
+
+			if (name.equals("quit")) {
+				start = false;
+				continue;
+			}
+
+			System.out.println("Input the type of food of the food truck:");
+			String type = scan.next();
+
+			System.out.println("Input the rating of the food truck:");
+			double rating = scan.nextDouble();
+
+			newft = new FoodTruck(name, type, rating);
+			addTruck(newft);
+			newft.setidNumber();
+
+			if (count == 5) {
+				start = false;
+			}
+		}
+		do {
+			System.out.println();
+			createMenu();
+			System.out.println();
+			int choice = scan.nextInt();
+			switch (choice) {
+
+			case 1:
+				listOf();
+				break;
+			case 2:
+				listAverage();
+				break;
+			case 3:
+				listHighest();
+				break;
+			case 4:
+				System.out.println("Thank you for using our app. Goodbye");
+				start = true;
+				break;
+			default:
+				System.out.println("Error");
+				break;
+
+			}
+		} while (!start);
+
+		scan.close();
 	}
-	public void createMenu() {
+
+	private void addTruck(FoodTruck newft) {
+		FleetOfFoodTruck[count] = newft;
+		count++;
+	}
+
+	private void createMenu() {
 		System.out.println("1. List all existing food trucks.");
 		System.out.println("2. See the average rating of food trucks.");
 		System.out.println("3. Display the highest-rated food truck.");
 		System.out.println("4. Quit the program.");
 
+	}
+
+	private void listOf() {
+		for (FoodTruck list : FleetOfFoodTruck) {
+			if (list != null) {
+				System.out.println(list.toString());
+			}
+		}
+	}
+
+	private void listAverage() {
+		double total = 0.0;
+		for (FoodTruck list : FleetOfFoodTruck) {
+			if (list != null) {
+				total += list.getRating();
+			}
+		}
+		double average = total / count;
+		System.out.println("The average rating of all Food Trucks is: " + average);
+	}
+
+	private void listHighest() {
+		double highest = 0.0;
+		for (FoodTruck list : FleetOfFoodTruck) {
+			if (list != null) {
+				if (highest < list.getRating()) {
+					highest = list.getRating();
+
+				}
+			}
+		}
+		for (FoodTruck list : FleetOfFoodTruck) {
+			if (list != null) {
+				if (highest == list.getRating()) {
+					System.out.println("The highest rating Food Truck is: " + list.toString());
+
+				}
+			}
+
+		}
 	}
 
 }
--- src/com/skilldistrillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistrillery/foodtruck/entities/FoodTruck.java
@@ -1,15 +1,77 @@
 package com.skilldistrillery.foodtruck.entities;
 
 public class FoodTruck {
+	private static int idNumber = 1;
+	private String name;
+	private String type;
+	private int iD;
+	private double rating;
 
+	/*
+	 * You will define a FoodTruck class with fields for a unique numeric id, a name
+	 * ("TacoRific", "Mediterranean Medic", etc.), food type ("Tacos", "Falafel",
+	 * etc.), and a numeric rating. Potentially use getters and setters
+	 * 
+	 */
 
-/*   You will define a FoodTruck class with fields for a unique numeric 
- * id, a name ("TacoRific", "Mediterranean Medic", etc.), food type 
- * ("Tacos", "Falafel", etc.), and a numeric rating.
- * Potentially use getters and setters
+	public FoodTruck() {
 
-*/
-	
-	
-	
+	}
+
+	public FoodTruck(String name, String type, double rating) {
+
+		this(idNumber, name, type, rating);
+	}
+
+	public FoodTruck(int iD, String name, String type, double rating) {
+		this.iD = iD;
+		this.name = name;
+		this.type = type;
+		this.rating = rating;
+	}
+
+	public int getiD() {
+		return iD;
+	}
+
+	public void setiD(int iD) {
+		this.iD = iD;
+	}
+
+	public void setidNumber() {
+		idNumber++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String toString() {
+		StringBuilder listTruck = new StringBuilder();
+		listTruck.append("Food Truck iD Number: ").append(iD).append("  Food Truck Name: ").append(name)
+				.append("  Type of Food: ").append(type).append("  Rating of FoodTruck: ").append(rating);
+
+		return listTruck.toString();
+	}
+
 }
